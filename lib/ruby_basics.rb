3a1ab4d33b4254e8341fa3e01772962b32bdfbe7
hai-nguyen1112 <hainguyen871112@gmail.com>
2018-12-23 15:10:47 -0500

    My todo ruby basics commit

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+ return num1 / num2
 end
-
+division(12, 2)
 def assign_variable(value)
-  
+ name = value
+ return name
 end
-
-def argue
-  
+assign_variable("Dan")
+def argue(argument)
+ return argument
 end
-
-def greeting
-
+argue(1)
+def greeting(greeting, name)
 end
-
+greeting("Hello", "Dan")
 def return_a_value
-
+phrase = "Nice"
 end
-
+return_a_value
 def last_evaluated_value
-
+"Nice"
+"expert"
 end
-
-def pizza_party
-
+last_evaluated_value
+def pizza_party(phrase = "cheese")
+return phrase
 end
+pizza_party
+pizza_party("tomato")
